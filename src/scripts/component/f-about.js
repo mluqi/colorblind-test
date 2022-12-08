70f676b064026eeb175d8434069e0467bcf1eb4a
Ardy wirasaputra <ardywsptr1@gmail.com>
2022-12-09 00:09:05 +0700

    update hover

--- src/scripts/component/f-about.js
+++ src/scripts/component/f-about.js
@@ -46,17 +46,17 @@ class FooterSection extends HTMLElement {
                         <!-- Grid column -->
                         <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
                             <h6 class="text-uppercase fw-bold mb-4">Developer</h6>
-                            <p>
-                                <a href="https://github.com/mluqi" class="hoverfooter text-reset">Mohammad Luqi Wiharto</a>
+                            <p class="hoverfooter">
+                                <a href="https://github.com/mluqi" class="text-reset">Mohammad Luqi Wiharto</a>
                             </p>
-                            <p>
-                                <a href="https://github.com/Ardywsptr" class="hoverfooter text-reset">Ardy Wirasaputra</a>
+                            <p class="hoverfooter">
+                                <a href="https://github.com/Ardywsptr" class="text-reset">Ardy Wirasaputra</a>
                             </p>
-                            <p>
-                                <a href="https://github.com/wiraaji" class="hoverfooter text-reset">Wira Aji Nugraha</a>
+                            <p class="hoverfooter">
+                                <a href="https://github.com/wiraaji" class="text-reset">Wira Aji Nugraha</a>
                             </p>
-                            <p>
-                                <a href="https://github.com/isamsd26" class="hoverfooter text-reset">Isa Mas'ud</a>
+                            <p class="hoverfooter">
+                                <a href="https://github.com/isamsd26" class="text-reset">Isa Mas'ud</a>
                             </p>
                         </div>
                         <!-- Grid column -->
@@ -64,20 +64,20 @@ class FooterSection extends HTMLElement {
                         <!-- Grid column -->
                         <div class=" col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
                             <h6 class="text-uppercase fw-bold mb-4">Useful links</h6>
-                            <p>
-                                <a href="/" class="hoverfooter text-reset">Home</a>
+                            <p class="hoverfooter">
+                                <a href="/" class="text-reset">Home</a>
                             </p>
-                            <p>
-                                <a href="#tips" class="hoverfooter text-reset">Tips & Trik</a>
+                            <p class="hoverfooter">
+                                <a href="#tips" class="text-reset">Tips & Trik</a>
                             </p>
-                            <p>
-                                <a href="#news" class="hoverfooter text-reset">Berita</a>
+                            <p class="hoverfooter">
+                                <a href="#news" class="text-reset">Berita</a>
                             </p>
-                            <p>
-                                <a href="#/tes" class="hoverfooter text-reset">Tes</a>
+                            <p class="hoverfooter">
+                                <a href="#/tes" class="text-reset">Tes</a>
                             </p>
-                            <p>
-                                <a href="#/riwayat" class="hoverfooter text-reset">Riwayat</a>
+                            <p class="hoverfooter">
+                                <a href="#/riwayat" class="text-reset">Riwayat</a>
                             </p>
                         </div>
                         <!-- Grid column -->
